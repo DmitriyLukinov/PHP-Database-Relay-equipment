3c22721f1cca77d5614c24c4f0dd4b614f723ea0
DmitriyLukinov <lukinovdmitriy@yahoo.com>
2023-12-28 12:32:20 +0200

    final

--- app/Http/Controllers/Message.php
+++ app/Http/Controllers/Message.php
@@ -5,12 +5,27 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Models\Substation;
+use Inertia\Inertia;
 
 class Message extends Controller
 {
-    public function print(Request $request )
+    public function showSubstations()
     {
-        $input = $request->input();
-        Log::info($input);
+        $substations = Substation::getSubstations();
+        return Inertia::render('Test', ['txt'=>$substations]);
+    }
+    public function showFiders(Request $req)
+    {
+        $data = $req->input('sbst');
+        $fiders = Substation::getFiders($data);
+
+        if($req->hasHeader('X-Requested-With')==true){
+            return ['data' => $fiders, 'url' => "/s?sbst=${data}"];
+        }
+        else{
+            Log::info('XMLHttpRequest');
+            return Inertia::render('Test', ['txt'=>$fiders]); 
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use App\Http\Controllers\Message;
-use Inertia\Inertia;
-use App\Models\Substation;
 
-Route::get('/', function () {
-    $substations = Substation::getSubstations();
-    return Inertia::render('Test', ['txt'=>$substations]); 
-}); 
 
-Route::get('/s', function(Request $req){
-    $data = $req->input('sbst');
-    $fiders = Substation::getFiders($data);
+Route::get('/', [Message::class, 'showSubstations']); 
+Route::get('/s', [Message::class, 'showFiders']);
 
-    if($req->ajax()==true){
-        return response()->json(['data' => $fiders, 'url' => "/s?sbst=${data}"]);
-    }
-    else{
-        Log::info('XMLHttpRequest');
-        return Inertia::render('Test', ['txt'=>$fiders]); 
-    }
-});
-
-//Route::inertia('/s', 'Auxi');
